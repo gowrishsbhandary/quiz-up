27b56fd15532ca762cc46b12c96ca880b5559386
gowrishsbhandary <gowrishsbhandary@gmail.com>
2021-07-31 18:08:02 +0300

    App setup

---
+++ src/main/java/com/cgm/quiz/up/QuizUpApplication.java
@@ -0,0 +1,13 @@
+package com.cgm.quiz.up;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cgm/quiz/up/QuizUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgm.quiz.up;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
